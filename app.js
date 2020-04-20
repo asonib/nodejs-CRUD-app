a7692a6ad8594d3e85f80a92708d6f0d31390246
Aryan Soni <aryan85tata@gmail.com>
2020-04-20 12:26:34 +0530

    added views and edit functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost/shoppingapp', () => {
     
@@ -36,6 +38,54 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/admin/edit/:id', (req, res) => {
+    RegisterModel.findById({
+        _id: req.params.id
+    })
+    .then((data) => {
+        console.log(data);
+        res.render('admin/editUser', {
+            user: data
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    }); 
+});
+app.put('/admin/edit/:id', (req, res) => {
+    RegisterModel.findById({
+        _id: req.params.id
+    })
+    .then((user) => {
+        user.name = req.body.name,
+        user.email = req.body.email,
+        user.phone = req.body.phone
+
+        user.save()
+        .then(() => {
+            console.log('Updated successfully');
+            res.redirect('/admin/viewUsers');
+        })
+        .catch((err) => {
+            console.log('error updating file');
+        });
+    })
+    .catch((err) => {
+        console.log('cannont find user');
+    });
+});
+
+app.delete('/admin/delete/:id', (req, res) => {
+    RegisterModel.findByIdAndDelete({
+        _id: req.params.id
+    })
+    .then(() => {
+        res.redirect('/admin/viewUsers');
+    })
+    .catch((err) => {
+        console.log('error deleting data');
+    })
+});
 
 port = process.env.port || 3000;
 app.listen(port, () => {
