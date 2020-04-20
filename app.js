b508f69bd88b14084c62ab995b80bf65243513ee
Aryan Soni <aryan85tata@gmail.com>
2020-04-20 08:54:44 +0530

    added templating engine and third party modules

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 3000;
-app.listen((port) => {
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+port = process.env.port ||3000;
+app.listen(port, () => {
     console.log(`server started on port ${port}`);
-})
+});
