e6b5e212c8c29caaff57a01e02bc0485fdbb5c35
Aryan Soni <aryan85tata@gmail.com>
2020-04-20 09:11:39 +0530

    added login route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,10 +10,28 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
+mongoose.connect('mongodb://localhost/shoppingapp', () => {
+    useUnifiedTopology: true
+    useNewUrlParser: true
+})
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch(() => {
+        console.log('error connecting to DB');
+    });
+;
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.send("ALL OK");
 });
 
 port = process.env.port ||3000;
