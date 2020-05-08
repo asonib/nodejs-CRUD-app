1fa928194caaeb0b6c00c1ee5b29401e6876cce5
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-05-08 19:51:28 +0530

    jwt samples

---
+++ samples/jwt/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+global.__basedir = __dirname;
+
+mongoose.connect('mongodb://localhost/mern', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((result) => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.log('Problem connecting to he database');
+});
+
+const config = require('./config/config');
+console.log("Encryptor : ", config.config.jwtSecret);
+app.get('/', (req, res) => {
+    payload = {
+        user: {
+            id: 'ASB75E'
+        }
+    }
+    jwt.sign({
+        data: payload
+    }, config.config.jwtSecret,
+        {
+            expiresIn: 60 * 60
+        }, (err, token) => {
+            if(err) throw err;
+            console.log(token);
+            res.render('index', {
+                title: 'Welcome | Dashboard',
+                token: token
+            });
+        }
+    );
+});
+app.get('/auth/dashboard', (req, res) => {
+    
+    // var decoded = jwt.verify(data, 'asb75E');
+    // console.log(decoded.user);
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, (result) => {
+    console.log(`server started at port ${port}`);
+});
